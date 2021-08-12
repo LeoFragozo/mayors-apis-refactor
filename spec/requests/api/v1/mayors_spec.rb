e36da841eb2fc05b2c47f346195c17237f20ffc8
Leonardo Fragozo <lqfragozo@gmail.com>
2021-08-12 20:43:49 -0300

    mayor specs refactored

--- spec/requests/api/v1/mayors_spec.rb
+++ spec/requests/api/v1/mayors_spec.rb
@@ -1,16 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Mayors', type: :request do
+  context 'mayor crud operations'
   let(:mayor) { create(:mayor) }
 
-  describe 'GET /index' do
-    it 'must return 200 http status' do
-      get '/api/v1/mayors'
-      expect(response).to have_http_status(:ok)
-    end
+  it '.create' do
+    get "/api/v1/mayors/#{mayor.id}"
+    expect(response).to have_http_status(200)
   end
 
-  it 'must return the current male mayor data' do
+  it '.show' do
+    get "/api/v1/mayors/#{mayor.id}"
+    expect(json_body[:data][:name]).to eq(mayor.name)
+    expect(json_body[:data][:gender]).to eq(mayor.gender)
+  end
+
+  it '.destroy' do
+    delete "/api/v1/mayors/#{mayor.id}"
+    expect(response.status).to eq(200)
+  end
+
+  it '.update' do
+    body_data = { mayor: { name: mayor.name, gender: mayor.gender } }
+    put "/api/v1/mayors/#{mayor.id}", params: body_data
+    expect(json_body[:message]).to eq('Prefeito/a atualizado/a')
+    expect(response.status).to eq(200)
+  end
+end
+
+context 'GET /index' do
+  it 'must return 200 http status' do
+    get '/api/v1/mayors'
+    expect(response).to have_http_status(:ok)
+  end
+end
+
+context 'mayor gender specs' do
+  let(:mayor) { create(:mayor) }
+
+  it 'gets male mayor data ' do
     create(:mayor, name: mayor.name, gender: 'Male')
     get '/api/v1/mayors?gender=Male'
     expect(json_body[:data][0]).to have_key(:id)
@@ -22,7 +50,7 @@ RSpec.describe 'Api::V1::Mayors', type: :request do
     expect(json_body[:data][0][:gender]).to eq('Male')
   end
 
-  it 'must return the current female mayor data' do
+  it 'gets female mayor data' do
     create(:mayor, name: mayor.name, gender: 'Female')
     get '/api/v1/mayors?gender=Female'
     expect(json_body[:data][0]).to have_key(:id)
@@ -32,36 +60,5 @@ RSpec.describe 'Api::V1::Mayors', type: :request do
     expect(json_body[:data][0]).to have_key(:updated_at)
     expect(json_body[:data][0][:name]).to eq(mayor.name)
     expect(json_body[:data][0][:gender]).to eq('Female')
-  end
-
-  it 'returns a 200 custom status code' do
-    mayor = create(:mayor)
-    get "/api/v1/mayors/#{mayor.id}"
-    expect(response).to have_http_status(200)
-  end
-
-  it 'must return mayor data' do
-    mayor = create(:mayor)
-    get "/api/v1/mayors/#{mayor.id}"
-    expect(json_body[:data][:name]).to eq(mayor.name)
-    expect(json_body[:data][:gender]).to eq(mayor.gender)
-  end
-end
-
-describe 'delete operations' do
-  it 'allow delete of mayors' do
-    mayor = create(:mayor)
-    delete "/api/v1/mayors/#{mayor.id}"
-    expect(response.status).to eq(200)
-  end
-end
-
-describe 'update operation' do
-  it 'successfully updates a mayor' do
-    mayor = create(:mayor)
-    body_data = { mayor: { name: mayor.name, gender: mayor.gender } }
-    put "/api/v1/mayors/#{mayor.id}", params: body_data
-    expect(json_body[:message]).to eq('Prefeito/a atualizado/a')
-    expect(response.status).to eq(200)
   end
 end
