86270c998e6d1ac2091fa143604dfc5c8d83b9ce
Leonardo Fragozo <lqfragozo@gmail.com>
2021-08-13 14:39:25 -0300

    specs refactor v1

--- spec/requests/api/v1/cities_spec.rb
+++ spec/requests/api/v1/cities_spec.rb
@@ -1,52 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Cities', type: :request do
-  let(:city) { create(:city) }
+  context 'city crud operations' do
+    let(:city) { create(:city) }
 
-  describe 'GET /index' do
+    it '.create' do
+      get "/api/v1/cities/#{city.id}"
+      expect(response).to have_http_status(200)
+    end
+
+    it '.show' do
+      get "/api/v1/cities/#{city.id}"
+      expect(json_body[:data][:name]).to eq(city.name)
+    end
+
+    it '.update' do
+      body_data = { city: { name: city.name } }
+      put "/api/v1/cities/#{city.id}", params: body_data
+      expect(json_body[:message]).to eq('Cidade atualizada')
+      expect(response.status).to eq(200)
+    end
+
+    it '.destroy' do
+      delete "/api/v1/cities/#{city.id}"
+      expect(response.status).to eq(200)
+    end
+  end
+
+  context 'GET /index' do
     it 'must return 200 http status' do
       get '/api/v1/cities'
       expect(response).to have_http_status(:ok)
     end
   end
 
-  it 'must return the current city data' do
-    create(:city)
-    get '/api/v1/cities'
-    expect(json_body[:data][0]).to have_key(:id)
-    expect(json_body[:data][0]).to have_key(:name)
-    expect(json_body[:data][0]).to have_key(:created_at)
-    expect(json_body[:data][0]).to have_key(:updated_at)
-    expect(json_body[:data][0][:name]).to eq(city.name)
-  end
-
-  it 'returns a 200 custom status code' do
-    city = create(:city)
-    get "/api/v1/cities/#{city.id}"
-    expect(response).to have_http_status(200)
-  end
-
-  it 'must return city data' do
-    city = create(:city)
-    get "/api/v1/cities/#{city.id}"
-    expect(json_body[:data][:name]).to eq(city.name)
-  end
-end
-
-describe 'delete operations' do
-  it 'allow delete of cities' do
-    city = create(:city)
-    delete "/api/v1/cities/#{city.id}"
-    expect(response.status).to eq(200)
-  end
-end
-
-describe 'update operation' do
-  it 'successfully updates a city' do
-    city = create(:city)
-    body_data = { city: { name: city.name } }
-    put "/api/v1/cities/#{city.id}", params: body_data
-    expect(json_body[:message]).to eq('Cidade atualizada')
-    expect(response.status).to eq(200)
+  context 'check city data' do
+    let(:city) { create(:city) }
+    it 'gets city data' do
+      create(:city)
+      get '/api/v1/cities'
+      expect(json_body[:data][0]).to have_key(:id)
+      expect(json_body[:data][0]).to have_key(:name)
+      expect(json_body[:data][0]).to have_key(:created_at)
+      expect(json_body[:data][0]).to have_key(:updated_at)
+      expect(json_body[:data][0][:name]).to eq(city.name)
+    end
   end
 end
--- spec/requests/api/v1/mayors_spec.rb
+++ spec/requests/api/v1/mayors_spec.rb
@@ -1,64 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Mayors', type: :request do
-  context 'mayor crud operations'
-  let(:mayor) { create(:mayor) }
+  context 'mayor crud operations' do
+    let(:mayor) { create(:mayor) }
 
-  it '.create' do
-    get "/api/v1/mayors/#{mayor.id}"
-    expect(response).to have_http_status(200)
+    it '.create' do
+      get "/api/v1/mayors/#{mayor.id}"
+      expect(response).to have_http_status(200)
+    end
+
+    it '.show' do
+      get "/api/v1/mayors/#{mayor.id}"
+      expect(json_body[:data][:name]).to eq(mayor.name)
+      expect(json_body[:data][:gender]).to eq(mayor.gender)
+    end
+
+    it '.destroy' do
+      delete "/api/v1/mayors/#{mayor.id}"
+      expect(response.status).to eq(200)
+    end
+
+    it '.update' do
+      body_data = { mayor: { name: mayor.name, gender: mayor.gender } }
+      put "/api/v1/mayors/#{mayor.id}", params: body_data
+      expect(json_body[:message]).to eq('Prefeito/a atualizado/a')
+      expect(response.status).to eq(200)
+    end
   end
 
-  it '.show' do
-    get "/api/v1/mayors/#{mayor.id}"
-    expect(json_body[:data][:name]).to eq(mayor.name)
-    expect(json_body[:data][:gender]).to eq(mayor.gender)
+  context 'GET /index' do
+    it 'must return 200 http status' do
+      get '/api/v1/mayors'
+      expect(response).to have_http_status(:ok)
+    end
   end
 
-  it '.destroy' do
-    delete "/api/v1/mayors/#{mayor.id}"
-    expect(response.status).to eq(200)
-  end
+  context 'mayor gender specs' do
+    let(:mayor) { create(:mayor) }
 
-  it '.update' do
-    body_data = { mayor: { name: mayor.name, gender: mayor.gender } }
-    put "/api/v1/mayors/#{mayor.id}", params: body_data
-    expect(json_body[:message]).to eq('Prefeito/a atualizado/a')
-    expect(response.status).to eq(200)
-  end
-end
+    it 'gets male mayor data ' do
+      create(:mayor, name: mayor.name, gender: 'Male')
+      get '/api/v1/mayors?gender=Male'
+      expect(json_body[:data][0]).to have_key(:id)
+      expect(json_body[:data][0]).to have_key(:name)
+      expect(json_body[:data][0]).to have_key(:gender)
+      expect(json_body[:data][0]).to have_key(:created_at)
+      expect(json_body[:data][0]).to have_key(:updated_at)
+      expect(json_body[:data][0][:name]).to eq(mayor.name)
+      expect(json_body[:data][0][:gender]).to eq('Male')
+    end
 
-context 'GET /index' do
-  it 'must return 200 http status' do
-    get '/api/v1/mayors'
-    expect(response).to have_http_status(:ok)
-  end
-end
-
-context 'mayor gender specs' do
-  let(:mayor) { create(:mayor) }
-
-  it 'gets male mayor data ' do
-    create(:mayor, name: mayor.name, gender: 'Male')
-    get '/api/v1/mayors?gender=Male'
-    expect(json_body[:data][0]).to have_key(:id)
-    expect(json_body[:data][0]).to have_key(:name)
-    expect(json_body[:data][0]).to have_key(:gender)
-    expect(json_body[:data][0]).to have_key(:created_at)
-    expect(json_body[:data][0]).to have_key(:updated_at)
-    expect(json_body[:data][0][:name]).to eq(mayor.name)
-    expect(json_body[:data][0][:gender]).to eq('Male')
-  end
-
-  it 'gets female mayor data' do
-    create(:mayor, name: mayor.name, gender: 'Female')
-    get '/api/v1/mayors?gender=Female'
-    expect(json_body[:data][0]).to have_key(:id)
-    expect(json_body[:data][0]).to have_key(:name)
-    expect(json_body[:data][0]).to have_key(:gender)
-    expect(json_body[:data][0]).to have_key(:created_at)
-    expect(json_body[:data][0]).to have_key(:updated_at)
-    expect(json_body[:data][0][:name]).to eq(mayor.name)
-    expect(json_body[:data][0][:gender]).to eq('Female')
+    it 'gets female mayor data' do
+      create(:mayor, name: mayor.name, gender: 'Female')
+      get '/api/v1/mayors?gender=Female'
+      expect(json_body[:data][0]).to have_key(:id)
+      expect(json_body[:data][0]).to have_key(:name)
+      expect(json_body[:data][0]).to have_key(:gender)
+      expect(json_body[:data][0]).to have_key(:created_at)
+      expect(json_body[:data][0]).to have_key(:updated_at)
+      expect(json_body[:data][0][:name]).to eq(mayor.name)
+      expect(json_body[:data][0][:gender]).to eq('Female')
+    end
   end
 end
